af5a4e65778b1a588076c494da6ef81c6fae5368
itasio <61111336+itasio@users.noreply.github.com>
2023-06-17 12:57:51 +0300

    initialize dicts

--- rlcard/agents/value_iteration_agent.py
+++ rlcard/agents/value_iteration_agent.py
@@ -1,7 +1,11 @@
 import pprint as pp
 import numpy as np
 import collections
+from scipy.special import softmax
+import os
+import pickle
 
+from rlcard.utils.utils import *
 '''
 P is the state space that we need for implementing value iteration
 P["state1","raise"] for example captures what happens if at state1 I take action: raise.
@@ -11,30 +15,30 @@ next state is about the next state the agent will be. It's not about the other p
 # P = {
 
 # "state1": {
-#     "raise": [(0.9, "state2", 0.0, 9),           # prob of next state(ctr/sum_of_ctrs_for_this_action), next state, reward of next state, ctr(num of time visited next state)
-#         (0.1, "state3", 0.5, 1)
+#     "raise": [  [0.9, "state2", 0.0, 9],           # prob of next state(ctr/sum_of_ctrs_for_this_action), next state, reward of next state, ctr(num of time visited next state)
+#                 [0.1, "state3", 0.5, 1]
 #     ],
-#     "check": [(0.1, "state2", 1.0, 1),
-#         (0.8, "state3", -1.0, 8),
-#         (0.1, "state4", 0.0, 1)
+#     "check": [ [0.1, "state2", 1.0, 1],
+#                [0.8, "state3", -1.0, 8],
+#                [0.1, "state4", 0.0, 1]
 #     ]
 # },
 
 # "state2": {
-#     "call": [(0.9, "state4", 0.0, 9), # prob of next state, next state, reward of next state, ctr(num of time visited next state)
-#         (0.1, "state3", 0.5, 1)
+#     "call": [[0.9, "state4", 0.0, 9], # prob of next state, next state, reward of next state, ctr(num of time visited next state)
+#         [0.1, "state3", 0.5, 1]
 #     ],
-#     "fold": [(0.1, "state3", 1.0, 1),
-#         (0.8, "state3", -1.0, 8),
-#         (0.1, "state4", 0.0, 1)
+#     "fold": [[0.1, "state3", 1.0, 1],
+#         [0.8, "state3", -1.0, 8],
+#         [0.1, "state4", 0.0, 1]
 #     ]
 # }
 
 # }
 
 class ValueIterAgent:
-    ''' A random agent. Random agents is for running toy examples on the card games
-        Initially will play some rounds randomly to estimate 
+    ''' An agent that will play according to value iteration algorithm,
+        in order to find optimal policy
     '''
 
     def __init__(self, env, model_path='./vi_model', e=0.8, conv_limit=1e-10):
@@ -50,7 +54,7 @@ class ValueIterAgent:
         self.agent_id = 0
         self.model_path = model_path
         self.iteration = 0
-        self.P = collections.__dict__
+        self.P = collections.__dict__               # state space
         self.V = collections.defaultdict(float)    # value function for each state
         self.Q = collections.defaultdict(list)     # Q table
     
@@ -70,8 +74,28 @@ class ValueIterAgent:
                 self.agent_id = id
                 break
 
-    def traverse_tree():
-        pass
+    def traverse_tree(self):
+        if self.env.is_over():
+            chips = self.env.get_payoffs()
+            return chips[self.agent_id]
+
+        current_player = self.env.get_player_id()
+        # compute the quality of previous state
+        if not current_player == self.agent_id:
+            state = self.env.get_state(current_player)
+            # other agent move
+            action = self.env.agents[current_player].step(state)
+
+            # Keep traversing the child state
+            self.env.step(action)
+            Vstate = self.traverse_tree()
+            self.env.step_back()
+            return Vstate
+        
+        if current_player == self.agent_id:
+            obs, legal_actions = self.get_state(current_player)
+            # if first time we encounter state initialize qualities
+            self.update_P_and_Q(obs, legal_actions, self.P, self.Q)
 
     @staticmethod
     def step(state):
@@ -105,20 +129,91 @@ class ValueIterAgent:
 
         return self.step(state), info
     
+    def update_P_and_Q(self, obs, legal_actions):
+        '''
+        For State Space P:
+            1) add new state and actions for it, or
+            2) update list of legal actions for existing state (add actions that are not already in the list)
+                    
+        For Q table:
+            1) Add new state and rewards for its legal actions(set to zero) or
+            2) Update rewards (set to zero) of legal actions for existing state 
+
+        Args:
+            obs (str): state_str
+            legal_actions (list): List of legel actions
+        '''
+        # if existing state
+        if obs in self.P.keys() and obs in self.Q.keys():
+            for action in legal_actions:            # check all listed actions that can be done in this state
+                if action not in self.P[obs].keys():    # if any not listed in P so far add it
+                    # initialize now will change later
+                    self.P[obs][action] = [[0,0,0,0]]   #prob_next_st, next_st, rew_next_st, num_visited_next_st
+                # now reset rewards of Q table for legal actions, will be recalculated 
+                self.Q[obs][action] = 0
+        else:
+            # new state found, add it to dicts and set appropriate values
+            self.Q[obs] = [-np.inf, -np.inf, -np.inf, -np.inf]
+            for action in legal_actions:
+                self.Q[obs][action] = 0
+            
+
+        if obs not in policy.keys() and obs not in self.qualities.keys():
+            tactions = np.array([-np.inf for action in range(self.env.num_actions)])
+            for action in range(self.env.num_actions):
+                if action in legal_actions:
+                    tactions[action] = 0
+            self.qualities[obs] = tactions
+            action_probs = softmax(tactions)
+            self.policy[obs] = action_probs
+        else:
+            action_probs = policy[obs].copy()
+        action_probs = remove_illegal(action_probs, legal_actions)
+    
+    def get_state(self, player_id):
+        ''' Get state_str of the player
+
+        Args:
+            player_id (int): The player id
+
+        Returns:
+            (tuple) that contains:
+                state (str): The state str
+                legal_actions (list): Indices of legal actions
+        '''
+        state = self.env.get_state(player_id)
+        return state['obs'].tostring(), list(state['legal_actions'].keys())
 
 if __name__ == '__main__':
     x = collections.defaultdict(list)
-    x[0]=1
-    x[1].append(2)
+    x["state1"].append(2)
+    x["state1"].append(3)
+    x["state1"].append(4)
+    x["state1"].append(2)
+
+    x["state2"].append(2)
+    x["state2"].append(5)
+    x["state2"].append(2)
+    x["state2"].append(6)
+    x["state2"][0]=10
+    x["ff"]=[]
+    ll = x.keys()
     print(x)
-    print(x[0])
-    print(x[1])
+    print(x.keys())
+    for i in x:
+        print(i)
+        print(x[i])
+        print(max(x[i]), x[i].index(max(x[i])))
+        # print max of each state and the index of it
+    # print(x)
+    # print(x["state1"])
+    # print(x["state2"])
     # Q = np.zeros((3, 4))
     # print(Q)
-    x[2]=3
-    print(x.keys())
-    print(x.values())
-    print(x.items())
+    # x[2]=3
+    # print(x.keys())
+    # print(x.values())
+    # print(x.items())
     # while True:
     #     for i in range(len(x)):
     #         print(x[i])
@@ -129,22 +224,22 @@ if __name__ == '__main__':
     P = {
 
     "state1": {
-        "raise": [(0.9, "state2", 0.0, 9),           # prob of next state(ctr/sum_of_ctrs_for_this_action), next state, reward of next state, ctr(num of time visited next state)
-            (0.1, "state3", 0.5, 1)
+        "raise": [[0.9, "state2", 0.0, 9],           # prob of next state(ctr/sum_of_ctrs_for_this_action), next state, reward of next state, ctr(num of time visited next state)
+            [0.1, "state3", 0.5, 1]
         ],
-        "check": [(0.1, "state2", 1.0, 1),
-            (0.8, "state3", -1.0, 8),
-            (0.1, "state4", 0.0, 1)
+        "check": [[0.1, "state2", 1.0, 1],
+            [0.8, "state3", -1.0, 8],
+            [0.1, "state4", 0.0, 1]
         ]
     },
 
     "state2": {
-        "call": [(0.9, "state4", 0.0, 9), # prob of next state, next state, reward of next state, ctr(num of time visited next state)
-            (0.1, "state3", 0.5, 1)
+        "call": [[0.9, "state4", 0.0, 9], # prob of next state, next state, reward of next state, ctr(num of time visited next state)
+            [0.1, "state3", 0.5, 1]
         ],
-        "fold": [(0.1, "state3", 1.0, 1),
-            (0.8, "state3", -1.0, 8),
-            (0.1, "state4", 0.0, 1)
+        "fold": [[0.1, "state3", 1.0, 1],
+            [0.8, "state3", -1.0, 8],
+            [0.1, "state4", 0.0, 1]
         ]
     }
 
@@ -152,7 +247,7 @@ if __name__ == '__main__':
     P["state3"] = 5
     print(P.keys())
     i = "state3"
-    if i in P.keys():
+    if i in P:
         print("yes")
     pp.pprint(P)
     k = P["state1"]["raise"]    # list
@@ -162,5 +257,15 @@ if __name__ == '__main__':
         print("yes")
     else:
         print("no")
-    Q = np.zeros((2, 4), dtype=np.float64)
-    print(Q)
+    if "bla" not in P["state1"].keys():
+        P["state1"]["bla"]=[[0,0,0,0]]
+    P["state1"]["bla"].append([1,1,1,1])
+    P["state1"]["bla"][0][0] = "hey"
+    pp.pprint(P)
+    # Q = np.zeros((2, 4), dtype=np.float64)
+    # Q[0][0] = 2
+    # Q[0][3] = 3
+    # Q[1][0] = 4
+
+    # print(np.max(Q, axis=1))   #returns the max of each row i.e. [3,4]
+    
